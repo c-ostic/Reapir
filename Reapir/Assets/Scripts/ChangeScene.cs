648543d72236fdad293357d6dd3042e1c383face
c-ostic <60513574+c-ostic@users.noreply.github.com>
2020-02-01 14:25:32 -0500

    Script for movement made

---
+++ Reapir/Assets/Scripts/ChangeScene.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ChangeScene : MonoBehaviour
+{
+    public string sceneName;
+
+    public bool active;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(active)
+            SceneManager.LoadScene(sceneName);
+    }
+}

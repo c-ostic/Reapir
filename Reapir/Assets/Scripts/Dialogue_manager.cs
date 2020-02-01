7d9afae6415d2776a4a3ba1c3c63160ba5565c1a
marshallDimperio <60161921+marshallDimperio@users.noreply.github.com>
2020-02-01 10:15:57 -0500

    dialogue works, now I just need to display it

--- Reapir/Assets/Scripts/Dialogue_manager.cs
+++ Reapir/Assets/Scripts/Dialogue_manager.cs
@@ -15,5 +15,34 @@ public class dialogue_manager : MonoBehaviour
     public void StartDialogue(dialogue text)
     {
         Debug.Log("Starting Conversation with" + text.name);
+
+        sentences.Clear();
+
+        foreach (string sentence in text.sentences)
+        {
+            sentences.Enqueue(sentence);
+        }
+
+        DisplayNextSentence();
+    }
+
+    public void DisplayNextSentence()
+    {
+        if (sentences.Count == 0)
+        {
+            EndDialogue();
+            return;
+        }
+
+        string sentence = sentences.Dequeue();
+        Debug.Log(sentence);
+    }
+
+    public void EndDialogue()
+    {
+        Debug.Log("End of conversation");
     }
 }
+
+
+

0f7784e969cc573f1e8b21d0e3b329786debbafe
marshallDimperio <60161921+marshallDimperio@users.noreply.github.com>
2020-02-01 20:07:50 -0500

    Working clone function

---
+++ Reapir/Assets/Scripts/glitchCloneSrc.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class glitchCloneSrc : MonoBehaviour
+{
+    public GameObject clone;
+    public GameObject self;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log("Collision");
+        clone.SetActive(true);
+        self.SetActive(false);
+        
+    }
+
+
+}

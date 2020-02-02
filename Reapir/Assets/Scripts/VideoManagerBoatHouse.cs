1c9503b183bd6a2e6c9ddec8af9cfea9cf44733b
c-ostic <60513574+c-ostic@users.noreply.github.com>
2020-02-02 14:49:46 -0500

    added boathouse video
    contains placeholder video for now

---
+++ Reapir/Assets/Scripts/VideoManagerBoatHouse.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+
+public class VideoManagerBoatHouse : MonoBehaviour
+{
+    public Camera camera;
+    private VideoPlayer player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = camera.GetComponent<VideoPlayer>();
+        player.loopPointReached += QuitGame;
+    }
+
+    public void QuitGame(VideoPlayer vp)
+    {
+        Debug.Log("Quitting Game");
+        Application.Quit();
+    }
+}
